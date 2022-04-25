e898ade707b9524ea28c14cd7eed01e21cd7fe58
chajuhui123 <chajuhui123@naver.com>
2022-04-25 23:28:45 +0900

    220425

---
+++ BAEKJOON/gold/220425_단어수학.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("../example.txt").toString();
+// let input = fs.readFileSync("/dev/stdin").toString().split(" "); // 백준 제출시
+
+const inputArr = input.split("\n");
+
+// 풀이 진행 중
