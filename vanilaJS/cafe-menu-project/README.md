b808e343404d109febe8a2276b3613fac4a99551
Juhee(Joy) <chajuhui123@naver.com>
2022-04-29 11:00:16 +0900

    220429 : cafe-menu-project 메뉴 수정 기능 추가 & 리팩토링

--- vanilaJS/cafe-menu-project/README.md
+++ vanilaJS/cafe-menu-project/README.md
@@ -9,6 +9,3 @@
   - [ ] 메뉴 삭제시 브라우저에서 제공하는 `confirm` 인터페이스를 활용한다.
 - [ ] 총 메뉴 갯수를 count 하여 상단에 보여준다.
 - [ ] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입되어야 한다.
-
-
-test
--- vanilaJS/cafe-menu-project/index.js
+++ vanilaJS/cafe-menu-project/index.js
@@ -7,29 +7,53 @@
 
 // ----------------------------------------
 
-// TO DO. 메뉴 추가
-// - [V] 메뉴의 이름을 입력받고 엔터키를 누르면 메뉴가 추가된다.
-// - [V] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입되어야 한다.
-// - [V] 총 메뉴 갯수를 count 하여 상단에 보여준다.
-// - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화된다.
-// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
-
 // querySelector 가 중복되어 자주 사용되므로, DOM Element 를 가져올때 관용적으로 쓰이는 $ 를 util 로 빼줌
 /** DOM Element QuerySelector */
-const $ = (selector) => document.querySelector(selector);
+const $ = selector => document.querySelector(selector);
 
 function App() {
+  // TO DO. 메뉴 수정
+  // 이벤트 위임이라는 개념을 활용한다. 동적으로 추가되는 요소는 부모에게 "이벤트를 위임"한다. <li> 를 추가하기 때문에 상위 부모인 <ul> 태그에 미리 이벤트를 "위임"해둔다.
+  // - [V] 메뉴 수정 버튼의 클릭 이벤트를 받으면, 메뉴 수정하는 모달이 뜬다.
+  // - [V] 모달창에 신규 메뉴명을 입력받고, 확인 버튼을 누르면 메뉴가 업데이트된다.
+
+  $("#espresso-menu-list").addEventListener("click", e => {
+    // 각 엘레먼트가 가지고 있는 고유값 class 값을 활용
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name"); // closest 메서드를 이용해 가까이 있는 Li 태그를 찾을 수 있음.
+      const updatedMenuName = prompt(
+        "메뉴명을 수정하세요",
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+    if (e.target.classList.contains("menu-remove-button")) {
+    }
+  });
+
+  // TO DO. 메뉴 추가
+  // - [V] 메뉴의 이름을 입력받고 확인 버튼을 누르면 메뉴가 추가된다.
+  // - [V] 메뉴의 이름을 입력받고 엔터키를 누르면 메뉴가 추가된다.
+  // - [V] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입되어야 한다.
+  // - [V] 총 메뉴 갯수를 count 하여 상단에 보여준다.
+  // - [V] 메뉴가 추가되고 나면, input은 빈 값으로 초기화된다.
+  // - [V] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
   // form 이 자동으로 전송하는 역할을 하기 때문에 엔터키 입력시 새로고침이 되므로, 이를 막아줘야 한다.
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
+  $("#espresso-menu-form").addEventListener("submit", e => {
     e.preventDefault();
   });
 
-  // 메뉴의 이름을 입력받기
-  $("#espresso-menu-name").addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-      const $espressoMenuName = $("#espresso-menu-name").value;
-      const menuItemTemplate = (espressoMenuName) => {
-        return `
+  const addEspressoMenuItem = () => {
+    if ($("#espresso-menu-name").value.replace(/\s/g, "").length == 0) {
+      alert("값을 입력해주세요.");
+      return;
+    }
+
+    const $espressoMenuName = $("#espresso-menu-name").value;
+
+    const menuItemTemplate = espressoMenuName => {
+      return `
       <li class="menu-list-item d-flex items-center py-2">
         <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
         <button
@@ -46,23 +70,30 @@ function App() {
         </button>
       </li>
     `;
-      };
-      // $("#espresso-menu-list").innerHTML = menuItemTemplate($espressoMenuName); // 기존값을 덮어쓰게 된다는 문제점.
-      $("#espresso-menu-list").insertAdjacentHTML(
-        "beforeend",
-        menuItemTemplate($espressoMenuName)
-      );
-      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length; // 메뉴리스트의 모든 li 갯수
-      $(".menu-count").innerText = `총 ${menuCount} 개`;
+    };
+    // $("#espresso-menu-list").innerHTML = menuItemTemplate($espressoMenuName); // 기존값을 덮어쓰게 된다는 문제점.
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate($espressoMenuName)
+    );
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length; // 메뉴리스트의 모든 li 갯수
+    $(".menu-count").innerText = `총 ${menuCount} 개`;
+    $("#espresso-menu-name").value = ""; // 입력 후 Input 초기화
+  };
+
+  $("#espresso-menu-submit-button").addEventListener("click", () => {
+    addEspressoMenuItem();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", e => {
+    if (e.key !== "Enter") {
+      return;
     }
+    addEspressoMenuItem();
   });
 }
 
 App(); // App 함수 실행
-
-// TO DO. 메뉴 수정
-// - [ ] 메뉴 수정 버튼의 클릭 이벤트를 받으면, 메뉴 수정하는 모달이 뜬다.
-// - [ ] 모달창에 신규 메뉴명을 입력받고, 확인 버튼을 누르면 메뉴가 업데이트된다.
 
 // TO DO. 메뉴 삭제
 // - [ ] 메뉴 삭제 버튼의 클릭 이벤트를 받으면, 재확인 모달이 뜬다.
