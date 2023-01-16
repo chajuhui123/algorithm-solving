81195687ac05d541ea3c51365fc61dd2d273d6e2
chajuhui123 <chajuhui123@naver.com>
2023-01-17 00:16:16 +0900

    solve: [230117] BOJ silver3 게임

--- BOJ/이진탐색/230113_게임.py
+++ BOJ/이진탐색/230113_게임.py
@@ -1,36 +1,45 @@
 # 1072
 
 import sys
+import decimal
 input = sys.stdin.readline
 
 def get_victory_percent(x, y):
-    return int(y/x * 100)
+    # return int(y/x * 100) 
+    return y * 100 // x 
+    # 부동소수점 오차로 인해 해당 return 문에 대해서만 정답 처리가 됨. 
+    # int( / ) 와 // 의 차이를 이해해야 할듯 하다.
+    # python float 다룰 때 주의!
 
 # 1. 게임 횟수 x, 이긴 게임 수 y, 승률 z
 x, y = map(int, input().split())
 z = get_victory_percent(x, y)
 
-# 2. 승률이 이미 100인 경우는 오르지 않음
-if z >= 99:
-    print(-1)
-    exit(0)
+answer = float('inf')
 
-answer = 0
 start = 1
 end = 1000000000 # 최대 10억이 들어올 수 있어서 end 값을 10억으로 초기화
 
+# 2. 범위 좁혀가며 이진 탐색 
 while start <= end:
-        # 3. 범위 좁혀가며 이진 탐색 
         mid = (start + end) // 2
 
-        # x, y 는 함께 증가
+        # x, y 는 함께 증가 => 게임 진행 판수
+        
+        # mid번 더 진행했을 때 원래의 승률이 더 크다면 
+        # start를 mid 이상으로 두고 다시 탐색한다.
         if get_victory_percent(x+mid, y+mid) <= z:
             start = mid + 1
         else:
+            # mid번 더 진행했을 때 처음 승률보다 크다면, answer은 현재 mid가 되고
+            # end를 mid 이하로 두고 다시 탐색한다.
             answer = mid
             end = mid - 1
 
-            print(answer)
+if answer == float('inf'): # 처음 진행했을 때보다 승률이 크게 변경되지 않은 경우
+    print(-1) 
+else:
+    print(answer)
 
 # IDEA
 # 해당 문제는 왜 이진 탐색으로 풀어야 할까? "1 ≤ X ≤ 1,000,000,000"
