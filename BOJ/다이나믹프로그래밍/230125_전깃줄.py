fd19117380acd21a3689f463ed06ed848cd9dc42
chajuhui123 <chajuhui123@naver.com>
2023-01-31 23:56:51 +0900

    solve: [230131] BOJ gold5 전깃줄

--- BOJ/다이나믹프로그래밍/230125_전깃줄.py
+++ BOJ/다이나믹프로그래밍/230125_전깃줄.py
@@ -5,34 +5,19 @@ input = sys.stdin.readline
 
 n = int(input())
 
-right_arr = []
-left_arr = []
-same_arr = []
-
-compare = 0
+lines = []
+dp = [0] * n
 
 for _ in range(n):
-  # 왼쪽 전봇대, 오른쪽 전봇대 대소 관계 비교   
-  left, right = map(int, input().split())
-  
-  if left < right:
-    right_arr.append((left, right))
-  elif left > right:
-    left_arr.append((left, right))
-  else:
-    same_arr.append((left, right))
+  A, B = map(int, input().split())
+  lines.append((A, B))
 
-target = right_arr if len(left_arr) < len(right_arr) else left_arr
-answer = len(target)
+lines.sort()
 
-exclude_same_item = []
-for item in target:
-    for same_item in same_arr:
-        if item[0] > same_item[0] or item[1] > same_item[0]:
-            exclude_same_item.append(same_item[0])
+for i in range(n):
+    for j in range(i): # 0 ~ i-1
+        if lines[i][1] > lines[j][1] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
 
-answer = answer - len(list(set(exclude_same_item)))
-print(answer)
-
-
-
+print(n - max(dp))
