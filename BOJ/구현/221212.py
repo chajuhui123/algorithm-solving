ca62648c26bdb237c6012b1fd20ebd573b181657
chajuhui123 <chajuhui123@naver.com>
2023-02-24 23:24:11 +0900

    improve: 과거 풀이 문제 주석 추가

--- BOJ/구현/221212.py
+++ BOJ/구현/221212.py
@@ -38,3 +38,9 @@ def count_delete_alphabet(given, alphabets):
                 count += 1
                 alphabets = alphabets[0 : index_target] + alphabets[index_target+len(target) :len(alphabets)]
     return count
+
+
+# 구현 문제로 보았다.
+# BANANA => B 1개, A 3개, N 2개 조합으로 만들 수 있는 모든 유니크한 Array를 구했다.
+# 모든 조합에 대해 탐색하며 alphabets에서 given에 대한 순열 조합을 제거할 때, 몇 번 제거 가능한지 return 한다.
+# 복잡도가 높지 않을까 고민했는데, enable_alphabets 으로 리턴되는 조합 갯수가 자연수로 정해져있기 때문에 (BANANA 값은 변경되는 게 아니니!), 괜찮을 것 같다고 생각...ㅎㅎㅎ
--- BOJ/그리디/221213.py
+++ BOJ/그리디/221213.py
@@ -1,5 +1,6 @@
 # target : string
 # cost : array
+
 def row_cost_non_neighboring(target, cost):
     target = list(target)
     answer = 0
@@ -27,3 +28,9 @@ def get_minimum_cost(arr):
         return sum(arr[0:len(arr) -1])
     else :
         return 0
+
+
+# '그리디 문제'로 분류해보았다.
+# 예를 들어 aaaabbcc 문자들이 중복되지 않도록하는데 적은 비용으로 제거해야한다면 
+# 중복되는 문자를 하나의 그룹으로 나눠서 본다 aaaa/bb/cc 이때 각 그룹에서 하나씩만 남기고 제거를 해야하는데,
+# 그렇다면 가장 큰 비용을 남기고 그 외 비용들을 더한다.
